caeb3ecf4d77f47121a6489ae12673b3d126f70e
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2023-06-25 15:37:36 +0200

    Update frontend to reflect new AWS links

--- frontend/src/app/environments/baseURLs.ts
+++ frontend/src/app/environments/baseURLs.ts
@@ -1,3 +1,3 @@
-export const baseURL = "https://5yk5ce9689.execute-api.eu-central-1.amazonaws.com/dev/"
-export const loginURL = "https://tim4-login.auth.eu-central-1.amazoncognito.com/signup?client_id=3k1p5k6bk4v93464brbk2a52r2&response_type=token&scope=email+openid&redirect_uri=http://localhost:4200/main"
+export const baseURL = "https://ycsvojtqma.execute-api.eu-central-1.amazonaws.com/dev/"
+export const loginURL = "https://team4-login.auth.eu-central-1.amazoncognito.com/signup?client_id=5r4advqgtjuqhcm1oukq1h6k37&response_type=token&scope=email+openid&redirect_uri=http://localhost:4200/main"
 
